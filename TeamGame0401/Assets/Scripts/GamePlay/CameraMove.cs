8eca75317234e45ea29e52f07b88468455adc2e2
stevenhu777 <48976756+stevenhu777@users.noreply.github.com>
2019-04-04 14:56:05 +0900

    Camera修正

--- TeamGame0401/Assets/Scripts/GamePlay/CameraMove.cs
+++ TeamGame0401/Assets/Scripts/GamePlay/CameraMove.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class CameraMove : MonoBehaviour
 {
+    public enum CameraTrigger
+    {
+        center,
+        left,
+        right,
+        up,
+        down
+    }
     //＊＊＊＊＊＊＊＊＊＊＊
     //Camera.main.fieldOfView　透視カメラの視野
     //Camera.main.orthographicSize  正交カメラの視野
@@ -12,12 +20,20 @@ public class CameraMove : MonoBehaviour
     public GameObject player1;
     public GameObject player2;
     public GameObject player3;
-    public GameObject player4;
+    //public GameObject player4;
+
+    private List<GameObject> players = new List<GameObject>();
+    private List<GameObject> playersCenter = new List<GameObject>();
     //カメラの移動速度
-    public float speed = 5.0f;
-    
+    public float speed =0.5f;
+
     //カメラフィールド
     private Camera thisCamera;
+
+    private Vector2 changePositiveView;
+    private Vector2 changeNegativeView;
+    private Vector2 changePositiveBorder;
+    private Vector2 changeNegativeBorder;
 
     //カメラ移動範囲を設定のため
 
@@ -27,48 +43,72 @@ public class CameraMove : MonoBehaviour
 
     private Collider2D rangeCol;
 
-    private bool isDecreaseScale;
+    private Vector3 offset;
 
-    private bool isInCreaseScale;
+    private bool isInView = true;
+    private bool isInBorder = false;
 
-    private float lastCameraScale;
+    //private bool isDecreaseScale;
+
+    //private bool isInCreaseScale;
+
+    //private float lastCameraScale;
 
     private float currentCameraScale;
-    public  void Awake()
+    public void Awake()
     {
         //プレイヤーの位置情報を取得
         //player = GameObject.FindGameObjectWithTag("Player").transform;
         //カメラを取得
         thisCamera = GetComponent<Camera>();
         //コライダーで範囲を設定
-        rangeCol = rangeGameObject.GetComponent<Collider2D>();      
+        rangeCol = rangeGameObject.GetComponent<Collider2D>();
     }
-    private bool IsInView(Vector3 worldPos)
+    private bool IsInView(Vector3 worldPos, Vector2 positiveNum, Vector2 negativeNum)
     {
         Transform camTransform = Camera.main.transform;
         Vector2 viewPos = Camera.main.WorldToViewportPoint(worldPos);
 
         Vector3 dir = (worldPos - camTransform.position).normalized;
+       // Debug.Log(negativeNum.x);
+        //float dot = Vector3.Dot(camTransform.forward, dir);
+        if (viewPos.x >= 0.08 + negativeNum.x && viewPos.x <= 0.92 + positiveNum.x &&
+            viewPos.y >= 0.08 + negativeNum.y && viewPos.y <= 0.92 + positiveNum.y)
+        {
 
-        float dot = Vector3.Dot(camTransform.forward, dir);
-        if (dot > 0 && viewPos.x >= 0.1&& viewPos.x <= 0.9&& viewPos.y >= 0.1 && viewPos.y <= 0.9)
             return true;
+        }
         else
+        {
             return false;
+        }
+
     }
     private bool IsInBorder(Vector3 position)
     {
         Vector2 viewPos = Camera.main.WorldToViewportPoint(position);
-        if (viewPos.x<=0.15f||viewPos.x>=0.85f||viewPos.y<=0.15f||viewPos.y>=0.85f)
+        if (viewPos.x <= 0.12f || viewPos.x >= 0.88f 
+            || viewPos.y <= 0.12f || viewPos.y >= 0.88f )
         {
             return true;
-
         }
         else
         {
             return false;
         }
     }
+    //private bool IsInWorldBorder(Vector3 position)
+    //{
+    //    if ((Mathf.Abs(position.x)>=8.1f)||(Mathf.Abs(position.y)>=4.1f))
+    //    {
+    //        return true;
+    //    }
+    //    else
+    //    {
+    //        return false;
+    //    }
+       
+    //}
     // Update is called once per frame
     private void FixedUpdate()
     {
@@ -97,96 +137,127 @@ public class CameraMove : MonoBehaviour
         //    }
         //}
 
-        ////プレイヤー位置を目標位置に設定
-        //Vector3 targetPos = new Vector3(player.position.x, player.position.y, transform.position.z);
-        ////今のカメラの位置を取得
-        //Vector3 cameraPos= transform.position;
-
-
-        //if (IsInView(player1.transform.position) && IsInView(player2.transform.position)
-        //  && IsInView(player3.transform.position) && IsInView(player4.transform.position))
+        if (player1 != null)
+        {
+            players.Add(player1);
+            playersCenter.Add(player1);
+        }
+        if (player2 != null)
+        {
+            players.Add(player2);
+            playersCenter.Add(player2);
+        }
+        if (player3 != null)
+        {
+            players.Add(player3);
+            playersCenter.Add(player3);
+        }
+        //if (player4!=null)
         //{
-        //    if (IsNotInBorder(player1.transform.position) && IsNotInBorder(player2.transform.position)
-        //     && IsNotInBorder(player3.transform.position) && IsNotInBorder(player4.transform.position))
+        //    players.Add(player4);
+        //}
+        for (int i = 0; i < players.Count; i++)
+        {
+            if (players[i].transform.position.x >= 8.1f)
+            {
+                changePositiveView.x = 0.1f;
+                //changeNegativeBorder.x = -0.05f;
+            }
+            if (players[i].transform.position.x <= -8.1f)
+            {
+                changeNegativeView.x = -0.1f;
+                //changePositiveBorder.x = 0.05f;
+            }
+            if (players[i].transform.position.y >= 4.1f)
+            {
+                changePositiveView.y = 0.1f;
+               // changeNegativeBorder.y = -0.05f;               
+            }
+            if (players[i].transform.position.y <= -4.1f)
+            {
+                changeNegativeView.y = -0.1f;
+                //changePositiveBorder.y = 0.05f;
+            }
+        }
+        if (players == null)
+        {
+            return;
+        }
+        for (int i = 0; i < players.Count; i++)
+        {
+            if (IsInView(players[i].transform.position, changePositiveView, changeNegativeView) == false)
+            {
+                IncreaseScale();
+                isInView = false;
+            }
+        }
+        if (isInView == true)
+        {
+            for (int j= 0; j < players.Count; j++)
+            {
+                if (IsInBorder(players[j].transform.position))
+                {
+                    isInBorder = true;
+                    break;
+                }
+            }
+            if (isInBorder==false)
+            {
+                DecreaseScale();
+            }
+            
+        }
+        //for (int i = 0; i < players.Count; i++)
+        //{
+        //    if (IsInView(players[i].transform.position)==false)
         //    {
-        //        if (Camera.main.orthographicSize>2)
+        //        if (Camera.main.orthographicSize < 5)
         //        {
-        //        Debug.Log("qweqwewe");
-        //        Debug.Log(Camera.main.WorldToViewportPoint(player1.transform.position));
-        //            Camera.main.orthographicSize = Mathf.Lerp(Camera.main.orthographicSize, 2, Time.deltaTime * speed);
+        //            Camera.main.orthographicSize = Mathf.Lerp(Camera.main.orthographicSize, 5, Time.deltaTime * 1);
+        //            //lastCameraScale = Camera.main.orthographicSize;
         //        }
+        //    }
+        //    else
+        //    {
+        //        Debug.Log("wewe");
+        //        for (int j = 0; j < players.Count; j++)
+        //        {
+        //            if (IsInBorder(players[j].transform.position))
+        //            {
+
+        //                return;
+        //            }
+        //        }
+        //        Camera.main.orthographicSize = Mathf.Lerp(Camera.main.orthographicSize, 2, Time.deltaTime * 1);
+        //        //lastCameraScale = Camera.main.orthographicSize;
+        //        return;
 
         //    }
-        ////}
+        //}
 
-        //do
-        //{
-        //    Camera.main.orthographicSize = Mathf.Lerp(Camera.main.orthographicSize, 2, Time.deltaTime * speed);
-        //} while ((IsInView(player1.transform.position) == false || IsInView(player2.transform.position) == false
-        //  || IsInView(player3.transform.position) == false || IsInView(player4.transform.position) == false));
+        isInView = true;
+        isInBorder = false;
 
-
-        if (IsInView(player1.transform.position) == false || IsInView(player2.transform.position) == false
-        || IsInView(player3.transform.position) == false || IsInView(player4.transform.position) == false)
+        changeNegativeView = Vector2.zero;
+        changePositiveView = Vector2.zero;
+        changePositiveBorder = Vector2.zero;
+        changeNegativeBorder = Vector2.zero;
+        Camera.main.orthographicSize = Mathf.Clamp(Camera.main.orthographicSize, 3, 5);
+    }
+    private void IncreaseScale()
+    {
+        if (Camera.main.orthographicSize < 5)
         {
-            //if (isDecreaseScale)
-            //{
-            //    currentCameraScale =Mathf.Lerp(currentCameraScale, 5, Time.deltaTime * speed);
-            //    Debug.Log(currentCameraScale);
-            //    Debug.Log(lastCameraScale);
-            //    if (currentCameraScale-lastCameraScale>=0.3f)
-            //    {
-            //        Camera.main.orthographicSize = currentCameraScale;
-            //        isDecreaseScale = false;
-            //    }
-            //}
-            //else
-            //{
-
-                if (Camera.main.orthographicSize <5)
-               {
-                    Camera.main.orthographicSize = Mathf.Lerp(Camera.main.orthographicSize,5, Time.deltaTime * 1);
-                    lastCameraScale = Camera.main.orthographicSize;
-                    isDecreaseScale = false;
-                    isInCreaseScale = true;
-                }
-            //}
-            
-            //else if (IsInBorder(player1.transform.position)&&IsInBorder(player2.transform.position)
-            //&&IsInBorder(player3.transform.position)&&IsInBorder(player4.transform.position))
-            //{
-            //    return;
-            //}
+            Camera.main.orthographicSize = Mathf.Lerp(Camera.main.orthographicSize, 5, Time.deltaTime * speed);
+            //lastCameraScale = Camera.main.orthographicSize;
         }
-        else
+    }
+    private void DecreaseScale()
+    {
+        if (Camera.main.orthographicSize > 3)
         {
-            //if (isInCreaseScale)
-            //{
-            //    currentCameraScale = Mathf.Lerp(currentCameraScale, 2, Time.deltaTime * speed);
-            //    if (lastCameraScale-currentCameraScale>=0.3f)
-            //    {
-            //       // Camera.main.orthographicSize = currentCameraScale;
-            //        isDecreaseScale = true;
-            //        isInCreaseScale = false;
-            //    }
-            //}
-            //else
-            //{
-                //Debug.Log("1");
-                if (IsInBorder(player1.transform.position)||IsInBorder(player2.transform.position)
-                    ||IsInBorder(player3.transform.position)||IsInBorder(player4.transform.position))
-                {
-                    return;
-                }
-                Camera.main.orthographicSize = Mathf.Lerp(Camera.main.orthographicSize, 2, Time.deltaTime * 1);
-                lastCameraScale = Camera.main.orthographicSize;
-                isDecreaseScale = true;
-                isInCreaseScale = false;
-           // }
-               
+            Camera.main.orthographicSize = Mathf.Lerp(Camera.main.orthographicSize, 3, Time.deltaTime * 1);
         }
-        //m_Camera.orthographicSize = Mathf.Lerp(m_Camera.orthographicSize, m_Camera.orthographicSize += speed / 2, Time.deltaTime * 30f);
-        Camera.main.orthographicSize = Mathf.Clamp(Camera.main.orthographicSize, 2, 5);
     }
     private void LateUpdate()
     {
@@ -200,14 +271,18 @@ public class CameraMove : MonoBehaviour
         //コライダー上の一番大きいの点
         Vector2 maxRange = rangeCol.bounds.max;
 
-        Vector3 offset = (player1.transform.position + player2.transform.position + player3.transform.position + player4.transform.position) / 4;
-
+        for (int i = 0; i < playersCenter.Count; i++)
+        {
+            offset += playersCenter[i].transform.position;
+        }
+        //offset = (player1.transform.position + player2.transform.position + player3.transform.position + player4.transform.position) / 4;
+        offset = offset / playersCenter.Count;
         offset.x = Mathf.Clamp(offset.x, minRange.x + cameraW, maxRange.x - cameraW);
 
         offset.y = Mathf.Clamp(offset.y, minRange.y + cameraH, maxRange.y - cameraH);
         //カメラ移動範囲（ｘ軸とｙ軸）を設定
-        transform.position =new Vector3(offset.x,offset.y,transform.position.z);
-        
+        transform.position = new Vector3(offset.x, offset.y, transform.position.z);
+
 
         ////カメラの移動条件を判定
         //if (targetPos.y > cameraPos.y + cameraH - 1 || targetPos.y < cameraPos.y - cameraH + 1
@@ -216,5 +291,4 @@ public class CameraMove : MonoBehaviour
         //    transform.position = Vector3.Lerp(cameraPos, targetPos, speed * Time.deltaTime);
         //}
     }
-
 }
